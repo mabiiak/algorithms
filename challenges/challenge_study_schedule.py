77659d6a627c3c0a47ee874eae068407efa410c1
mabiiak <mabiane.polniak@gmail.com>
2022-09-27 11:30:59 -0300

    fix: linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,9 +7,8 @@ def study_schedule(permanence_period, target_time):
     for entrance, exit in permanence_period:
         if type(entrance) is not int or type(exit) is not int:
             return None
-        
+
         if entrance <= target_time <= exit:
             count += 1
 
     return count
-        
