bf724cded26631a92434cce22383aa29354c8b88
mabiiak <mabiane.polniak@gmail.com>
2022-09-27 18:09:00 -0300

    feat: implement palindromes iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    comum = list(word)
+    contrary = []
+
+    if not word:
+        return False
+    else:
+        for letter in enumerate(word):
+            remove = comum.pop()
+            contrary.append(remove)
+
+        if contrary == list(word):
+            return True
+        else:
+            return False
