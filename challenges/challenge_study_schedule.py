f27b8f87c678085683d8e09778626d0a87d7af43
mabiiak <mabiane.polniak@gmail.com>
2022-09-27 10:27:57 -0300

    feat: implement study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count = 0
+
+    if not target_time:
+        return None
+
+    for entrance, exit in permanence_period:
+        if type(entrance) is not int or type(exit) is not int:
+            return None
+        
+        if entrance <= target_time <= exit:
+            count += 1
+
+    return count
+        
