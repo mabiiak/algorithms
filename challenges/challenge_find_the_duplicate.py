5472099bd7e9b7f6cc98c1dfbf604a2c661ce44f
mabiiak <mabiane.polniak@gmail.com>
2022-09-27 12:25:57 -0300

    feat: implement find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    nums.sort()
+    compar = 0
+
+    for i, num in enumerate(nums):
+        if (type(num) == str):
+            return False
+        if num < 0:
+            return False
+
+        if num == compar:
+            return num
+        else:
+            compar = num
+
+    return False
